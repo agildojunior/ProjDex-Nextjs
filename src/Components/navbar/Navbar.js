baeb6ac6b965cedad8cbe7758508877c41d67122
agildojunior <agildo500@hotmail.com>
2022-08-28 23:55:40 -0300

    criando Navbar

--- src/Components/navbar/Navbar.js
+++ src/Components/navbar/Navbar.js
@@ -1,18 +1,36 @@
 import Link from 'next/link'
+import styles from './Navbar.module.css'
 
 export default function Navbar() {
     return(
-        <ul>
-            <li>
-                <Link href="#">
-                    <a>link1</a>
-                </Link>
-            </li>
-            <li>
-                <Link href="#">
-                    <a>link2</a>
-                </Link>
-            </li>
-        </ul>
+            <ul className={styles.navegador}>
+                
+                <div className={styles.navegadordiv1}>
+                    <li className={styles.linav}>
+                        <Link href="#">
+                            <a className={styles.anav}>Inicio</a>
+                        </Link>
+                    </li>
+                </div>
+
+                <div className={styles.navegadordiv2}>
+                    <li className={styles.linav}>
+                        <Link href="#">
+                            <a className={styles.anav}>Info</a>
+                        </Link>
+                    </li>
+                    <li className={styles.linav}>
+                        <Link href="#">
+                            <a className={styles.anav}>Times</a>
+                        </Link>
+                    </li>
+                    <li className={styles.linav}>
+                        <Link href="#">
+                            <a className={styles.anav}>Sobre</a>
+                        </Link>
+                    </li>
+                </div>
+                
+            </ul>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import img from '../../public/image/PokemikioLogo.png'
 import Navbar from '../Components/navbar/Navbar'
 
 export default function Home() {
   return (
     <div className={styles.divprincipal}>
       <div className={styles.BackgroundDex}>
+        <Navbar></Navbar>
         <div className={styles.container920}>
           <a>aaa</a>
           <a>aaa</a>
@@ -56,7 +58,9 @@ export default function Home() {
           <a>aaa</a>
           <a>aaa</a>
           <a>aaa</a>
-          <a>aaa</a>
+        </div>
+        <div className={styles.logoPrincipal}>
+          <Image src={img} width='820px' height='350px'  />
         </div>
       </div>
     </div>
