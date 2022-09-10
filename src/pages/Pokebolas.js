996beb2bf36487f59510edbc35f2ca9f28988426
agildojunior <agildo500@hotmail.com>
2022-09-10 15:39:24 -0300

    Pokebolas adicionadas

--- src/pages/Pokebolas.js
+++ src/pages/Pokebolas.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import img from '../../public/image/PokemikioLogo.png'
+import imgpokebolas from '../../public/image/pokebolas.png'
 import Navbar from '../Components/navbar/Navbar'
 
 
@@ -12,11 +13,12 @@ export default function Pokebolas() {
       <div className={styles.BackgroundDex}>
         <Navbar></Navbar>
         <div className={styles.container920}>
-          <h1>Pokebolas</h1>
+          <h1 className={styles.titulobranco}>Pokebolas</h1>
+          <Image src={imgpokebolas} width='880px' height='500px'  />
 
         </div>
         <div className={styles.logoPrincipal}>
-          <Image src={img} width='580px' height='260px'  />
+          <Image src={img} width='580px' height='260px'/>
         </div>
       </div>
     </div>
